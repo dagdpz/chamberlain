129a4a4dc8224171778d085f0e4055435497e4a4
lubavas <19555010+lubavas@users.noreply.github.com>
2024-09-02 17:11:12 +0200

    fix grid hole coordinate in title

--- CL_plot_electrode_localization.m
+++ CL_plot_electrode_localization.m
@@ -44,23 +44,23 @@ xyz_identifier(:,3) = xyz(:,3) + z_offset_mm;
 h_fig = get(0,'Children');
 for f = 1:length(h_fig),
     set(0,'CurrentFigure',h_fig(f));
-    UD = get(h_fig(f),'UserData');
+    UD(f) = get(h_fig(f),'UserData');
     current_handles=findobj(gca,'Tag','penetration marker');
     xz_current_slice=[get(current_handles, 'XData') get(current_handles, 'YData')];
     if isa(xz_current_slice,'cell'); xz_current_slice=cell2mat(xz_current_slice); end
-    grid_z= (xz_current_slice(:,2) - fix(UD.voxel_dim/2))*UD.voxel_size*-1;
+    grid_z= (xz_current_slice(:,2) - fix(UD(f).voxel_dim/2))*UD(f).voxel_size*-1;
     switch saggital_or_coronal
         case 'coronal'
-            grid_x= (xz_current_slice(:,1) - fix(UD.voxel_dim/2))*UD.voxel_size/grid_spacing;
-            grid_y=repmat(UD.y_mm/grid_spacing,size(grid_x));
+            grid_x= (xz_current_slice(:,1) - fix(UD(f).voxel_dim/2))*UD(f).voxel_size/grid_spacing;
+            grid_y=repmat(UD(f).y_mm/grid_spacing,size(grid_x));
         case 'sagittal'
-            grid_y= (xz_current_slice(:,1) - fix(UD.voxel_dim/2))*UD.voxel_size/grid_spacing;
-            grid_x=repmat(UD.x_mm/grid_spacing,size(grid_y));
+            grid_y= (xz_current_slice(:,1) - fix(UD(f).voxel_dim/2))*UD(f).voxel_size/grid_spacing;
+            grid_x=repmat(UD(f).x_mm/grid_spacing,size(grid_y));
     end
     current_locations=ismember(round(xyz_identifier*1000),round([grid_x grid_y grid_z]*1000),'rows');
-    unique_electrode_positions=unique(xyz_nojitter(current_locations,1))*grid_spacing/UD.voxel_size + fix(UD.voxel_dim/2);
+    unique_electrode_positions=unique(xyz_nojitter(current_locations,1))*grid_spacing/UD(f).voxel_size + fix(UD(f).voxel_dim/2);
     for p=unique_electrode_positions'
-        line([p p],[0 UD.voxel_dim],'color','w','linestyle',':');
+        line([p p],[0 UD(f).voxel_dim],'color','w','linestyle',':');
     end
     %delete(current_handles);
 end
@@ -73,7 +73,7 @@ end
 
 for f = 1:length(h_fig),
     set(0,'CurrentFigure',h_fig(f));
-    UD = get(h_fig(f),'UserData');
+    UD(f) = get(h_fig(f),'UserData');
     set(findobj(gca,'Tag','penetration marker'),'Tag','penetration marker nonsignificant');
 end
 
@@ -87,7 +87,7 @@ for c=1:size(significant,2)
     h_fig = get(0,'Children');
     for f = 1:length(h_fig),
         set(0,'CurrentFigure',h_fig(f));
-        UD = get(h_fig(f),'UserData');
+        UD(f) = get(h_fig(f),'UserData');
         set(findobj(gca,'Tag','penetration marker'),'Tag',['penetration marker ' num2str(c)]);
     end
 end
@@ -123,17 +123,18 @@ for f = 1:length(h_fig),
     set(0,'CurrentFigure',h_fig(f));
     switch saggital_or_coronal
         case 'coronal'
-            y_mm = UD.y_mm;
+            y_mm = UD(f).y_mm;
             this_slice_sites_idx = find(xyz(:,2)*grid_spacing==y_mm);
         case 'sagittal'
-            x_mm = UD.x_mm;
+            x_mm = UD(f).x_mm;
             this_slice_sites_idx = find(xyz(:,1)*grid_spacing==x_mm);
     end
     
     n_unique_sites_this_slice = size(unique(xyz(this_slice_sites_idx,:),'rows'),1);
     
     old_str = get(get(gca,'Title'),'String');
-    title({char(old_str),sprintf('%s, %s (...,%d), %d sites %d unique sites',experiment_id,grid_id,xyz(this_slice_sites_idx(1),2),length(this_slice_sites_idx),n_unique_sites_this_slice)},'Interpreter','none');
+    gridhole_y = xyz(this_slice_sites_idx(1),2);
+    title({char(old_str),sprintf('%s, %s (...,%s), %d sites %d unique sites',experiment_id,grid_id,num2str(gridhole_y),length(this_slice_sites_idx),n_unique_sites_this_slice)},'Interpreter','none');
     
     % 	set(findobj(gca,'Tag','penetration marker'),'Color',penetration_marker_color);
     for c=1:size(significant,2)
